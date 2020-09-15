85576087293afd5daad56de62c03d13533d52c26
GRAT <gregorytyshchenko3011@gmail.com>
2020-09-15 11:44:07 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ class App extends React.Component {
 						{
 							!!image &&
 							<Div style={{ textAlign: 'center' }}>
-                                <img src={ image } alt="remote file" referrerPolicy="no-referrer" />
+                                <img src={ image } alt="remote file" />
 							</Div>
 						}
                     </Group>
